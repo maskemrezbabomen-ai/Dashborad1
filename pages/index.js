a9f80c886a916eacb8c4a9769c89d9b3871c1be6
maskemrezbabomen-ai <maskemrezbabomen@gmail.com>
2026-09-13 00:24:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -228,7 +228,7 @@ export default function Dashboard() {
     width: '80%',
     height: '1px',
     background: 'rgba(255,255,255,0.15)',
-    margin: '-10px auto 0',
+    margin: '8px auto 0',
   }}
 ></div>
 
