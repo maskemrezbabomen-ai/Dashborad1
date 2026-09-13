44de9b01624dce1edeb42351dea1193794ad8ac4
maskemrezbabomen-ai <maskemrezbabomen@gmail.com>
2026-09-13 13:21:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -253,7 +253,7 @@ export default function Dashboard() {
         justifyContent: 'center',
         gap: '9px',
         position: 'relative',
-        top: '-30px',
+        top: '0px',
       }}
     >
       <img
