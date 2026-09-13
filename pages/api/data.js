4582b362492e99504be6577ad62134c87fb628b1
maskemrezbabomen-ai <maskemrezbabomen@gmail.com>
2026-09-13 14:15:12 +0300

    Update data.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -51,11 +51,11 @@ export default async function handler(req, res) {
     try {
       const current = (await kv.get('app-data')) || DEFAULTS;
 
-      current[key] = value;
+current[key] = value;
 
-      await kv.get('app-data'))
+await kv.set('app-data', current);
 
-      return res.status(200).json({
+return res.status(200).json({
         ok: true,
       });
     } catch (e) {
