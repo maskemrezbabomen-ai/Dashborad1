b0f63475a1f74a83ef9c1fa2db35a5873ddc21f0
maskemrezbabomen-ai <maskemrezbabomen@gmail.com>
2026-09-13 16:01:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -230,9 +230,9 @@ return (
       </div>
 <div
   style={{
-    margin: '-1px 22px 18px',
+    margin: '10px 22px 18px',
     color: 'rgba(255,255,255,0.45)',
-    fontSize: '8px',
+    fontSize: '10px',
     fontWeight: 600,
     letterSpacing: '1.5px',
   }}
